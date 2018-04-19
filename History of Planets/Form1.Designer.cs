6af59104249fcb824902aebcdda7e6fdee438563
Lastmentor <riza.gs1905@hotmail.com>
2018-04-19 13:06:01 +0300

    Project Added

---
+++ History of Planets/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Student_Login_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonHome = new System.Windows.Forms.Button();
+            this.buttonPlanets = new System.Windows.Forms.Button();
+            this.buttonNews = new System.Windows.Forms.Button();
+            this.buttonSolarSystem = new System.Windows.Forms.Button();
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(461, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(212, 164);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 30F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(424, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(283, 49);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "History of Planets";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonSolarSystem);
+            this.panel1.Controls.Add(this.buttonNews);
+            this.panel1.Controls.Add(this.buttonPlanets);
+            this.panel1.Controls.Add(this.buttonHome);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 537);
+            this.panel1.TabIndex = 2;
+            // 
+            // buttonHome
+            // 
+            this.buttonHome.FlatAppearance.BorderSize = 0;
+            this.buttonHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonHome.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonHome.ForeColor = System.Drawing.Color.White;
+            this.buttonHome.Image = ((System.Drawing.Image)(resources.GetObject("buttonHome.Image")));
+            this.buttonHome.Location = new System.Drawing.Point(3, 1);
+            this.buttonHome.Name = "buttonHome";
+            this.buttonHome.Size = new System.Drawing.Size(194, 117);
+            this.buttonHome.TabIndex = 3;
+            this.buttonHome.Text = "Home";
+            this.buttonHome.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonHome.UseVisualStyleBackColor = true;
+            this.buttonHome.Click += new System.EventHandler(this.buttonHome_Click);
+            // 
+            // buttonPlanets
+            // 
+            this.buttonPlanets.FlatAppearance.BorderSize = 0;
+            this.buttonPlanets.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPlanets.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonPlanets.ForeColor = System.Drawing.Color.White;
+            this.buttonPlanets.Image = ((System.Drawing.Image)(resources.GetObject("buttonPlanets.Image")));
+            this.buttonPlanets.Location = new System.Drawing.Point(3, 276);
+            this.buttonPlanets.Name = "buttonPlanets";
+            this.buttonPlanets.Size = new System.Drawing.Size(194, 117);
+            this.buttonPlanets.TabIndex = 4;
+            this.buttonPlanets.Text = "Planets";
+            this.buttonPlanets.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonPlanets.UseVisualStyleBackColor = true;
+            this.buttonPlanets.Click += new System.EventHandler(this.buttonPlanets_Click);
+            // 
+            // buttonNews
+            // 
+            this.buttonNews.FlatAppearance.BorderSize = 0;
+            this.buttonNews.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNews.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonNews.ForeColor = System.Drawing.Color.White;
+            this.buttonNews.Image = ((System.Drawing.Image)(resources.GetObject("buttonNews.Image")));
+            this.buttonNews.Location = new System.Drawing.Point(3, 419);
+            this.buttonNews.Name = "buttonNews";
+            this.buttonNews.Size = new System.Drawing.Size(194, 117);
+            this.buttonNews.TabIndex = 5;
+            this.buttonNews.Text = "News";
+            this.buttonNews.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonNews.UseVisualStyleBackColor = true;
+            this.buttonNews.Click += new System.EventHandler(this.buttonNews_Click);
+            // 
+            // buttonSolarSystem
+            // 
+            this.buttonSolarSystem.FlatAppearance.BorderSize = 0;
+            this.buttonSolarSystem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSolarSystem.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonSolarSystem.ForeColor = System.Drawing.Color.White;
+            this.buttonSolarSystem.Image = ((System.Drawing.Image)(resources.GetObject("buttonSolarSystem.Image")));
+            this.buttonSolarSystem.Location = new System.Drawing.Point(3, 137);
+            this.buttonSolarSystem.Name = "buttonSolarSystem";
+            this.buttonSolarSystem.Size = new System.Drawing.Size(194, 117);
+            this.buttonSolarSystem.TabIndex = 6;
+            this.buttonSolarSystem.Text = "Solar System";
+            this.buttonSolarSystem.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonSolarSystem.UseVisualStyleBackColor = true;
+            this.buttonSolarSystem.Click += new System.EventHandler(this.buttonSolarSystem_Click);
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.BackColor = System.Drawing.Color.Gold;
+            this.panelLeft.Location = new System.Drawing.Point(199, 1);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(10, 117);
+            this.panelLeft.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Monotype Corsiva", 16F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(223)))), ((int)(((byte)(50)))));
+            this.textBox1.Location = new System.Drawing.Point(213, 252);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(718, 279);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(860, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 50);
+            this.button1.TabIndex = 6;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(934, 537);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panelLeft);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonNews;
+        private System.Windows.Forms.Button buttonPlanets;
+        private System.Windows.Forms.Button buttonHome;
+        private System.Windows.Forms.Button buttonSolarSystem;
+        private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ History of Planets/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_Login_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            panelLeft.Height = buttonHome.Height;
+            panelLeft.Top = buttonHome.Top;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonHome_Click(object sender, EventArgs e)
+        {
+            panelLeft.Height = buttonHome.Height;
+            panelLeft.Top = buttonHome.Top;
+        }
+
+        private void buttonSolarSystem_Click(object sender, EventArgs e)
+        {
+            panelLeft.Height = buttonSolarSystem.Height;
+            panelLeft.Top = buttonSolarSystem.Top;
+        }
+
+        private void buttonPlanets_Click(object sender, EventArgs e)
+        {
+            panelLeft.Height = buttonPlanets.Height;
+            panelLeft.Top = buttonPlanets.Top;
+        }
+
+        private void buttonNews_Click(object sender, EventArgs e)
+        {
+            panelLeft.Height = buttonNews.Height;
+            panelLeft.Top = buttonNews.Top;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
